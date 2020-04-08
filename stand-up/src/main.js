e22e64fcd11075734384d38ae2a06d13f2b292ad
Mike Cluck <mike-cluck@pluralsight.com>
2020-04-08 14:08:14 -0600

    Add stand-up order tool

---
+++ stand-up/src/main.js
@@ -0,0 +1,20 @@
+const thingsEl = document.getElementById('things')
+
+const loadData = name => fetch(`./data/${name}.json`).then(resp => resp.json())
+
+const main = async () => {
+  const now = new Date()
+  const seed = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`
+  const rng = new Math.seedrandom(seed)
+  const data = await loadData('ps-stand-up')
+  console.log(seed)
+  data.sort(() => (rng() > 0.5 ? 1 : -1))
+
+  for (const thing of data) {
+    const el = document.createElement('div')
+    el.innerText = thing
+    thingsEl.appendChild(el)
+  }
+}
+
+main()
