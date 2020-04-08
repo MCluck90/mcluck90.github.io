a52fd2f4a748257ff7553f48f5f0a99a52e2b7ba
Mike Cluck <mike-cluck@pluralsight.com>
2020-04-08 14:19:48 -0600

    Improve shuffling

--- stand-up/src/main.js
+++ stand-up/src/main.js
@@ -2,12 +2,31 @@ const thingsEl = document.getElementById('things')
 
 const loadData = name => fetch(`./data/${name}.json`).then(resp => resp.json())
 
+const shuffle = function (rng, array) {
+  let currentIndex = array.length
+  let temporaryValue, randomIndex
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(rng() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
 const main = async () => {
   const now = new Date()
   const seed = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`
   const rng = new Math.seedrandom(seed)
   const data = await loadData('ps-stand-up')
-  data.sort(() => (rng() > 0.5 ? 1 : -1))
+  shuffle(rng, data)
 
   for (const thing of data) {
     const el = document.createElement('div')
