698c207d961d1e32b52ba42218395a77484df3f2
Mike Cluck <mcluck90@gmail.com>
2021-12-24 14:48:22 -0600

    Update Boids

--- boids/dist/boids.js
+++ boids/dist/boids.js
@@ -1,4 +1,73 @@
 (() => {
+  // src/canvas.ts
+  var canvas = document.querySelector("canvas");
+  document.addEventListener("resize", () => {
+  });
+  canvas.width = canvas.getBoundingClientRect().width;
+  canvas.height = canvas.getBoundingClientRect().height;
+  var ctx = canvas.getContext("2d");
+  function rotatePoint(point, center, angle) {
+    const sin = Math.sin(angle);
+    const cos = Math.cos(angle);
+    point.x -= center.x;
+    point.y -= center.y;
+    const xNew = point.x * cos - point.y * sin;
+    const yNew = point.x * sin + point.y * cos;
+    point.x = xNew + center.x;
+    point.y = yNew + center.y;
+  }
+  var lerp = (n1, n2, amount) => {
+    amount = Math.max(amount, 0);
+    amount = Math.min(amount, 1);
+    return n1 + (n2 - n1) * amount;
+  };
+  var previousPositions = {};
+  var previousVelocities = {};
+  var drawBoid = (boid, size, color, allowWrapping) => {
+    const previousPosition = previousPositions[boid.id] || {
+      x: boid.velocity.x,
+      y: boid.velocity.y
+    };
+    previousPositions[boid.id] = previousPosition;
+    const previousVelocity = previousVelocities[boid.id] || {
+      x: boid.velocity.x,
+      y: boid.velocity.y
+    };
+    previousVelocities[boid.id] = previousVelocity;
+    previousPosition.x = lerp(previousPosition.x, boid.position.x, 0.1);
+    previousPosition.y = lerp(previousPosition.y, boid.position.y, 0.1);
+    const position = allowWrapping ? boid.position : previousPosition;
+    previousVelocity.x = lerp(previousVelocity.x, boid.velocity.x, 0.1);
+    previousVelocity.y = lerp(previousVelocity.y, boid.velocity.y, 0.1);
+    const direction = Math.atan2(previousVelocity.y, previousVelocity.x);
+    const tip = {
+      x: position.x + size,
+      y: position.y
+    };
+    const leftTail = {
+      x: position.x - size / 2,
+      y: position.y - size / 2
+    };
+    const rightTail = {
+      x: position.x - size / 2,
+      y: position.y + size / 2
+    };
+    rotatePoint(tip, position, direction);
+    rotatePoint(leftTail, position, direction);
+    rotatePoint(rightTail, position, direction);
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(tip.x, tip.y);
+    ctx.lineTo(leftTail.x, leftTail.y);
+    ctx.lineTo(rightTail.x, rightTail.y);
+    ctx.lineTo(tip.x, tip.y);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+  };
+
   // src/vector.ts
   var Vector = class {
     constructor(x = 0, y = 0) {
@@ -26,27 +95,37 @@
     div(n) {
       return new Vector(this.x / n, this.y / n);
     }
+    clone() {
+      return new Vector(this.x, this.y);
+    }
   };
 
   // src/boids.ts
+  function uuidv4() {
+    return ([1e7].toString() + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (Number(c) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> Number(c) / 4).toString(16));
+  }
   var createBoid = (x, y) => {
     const degrees = Math.random() * 360;
     const radians = degrees * (Math.PI / 180);
+    const position = new Vector(x, y);
+    const velocity = new Vector(Math.cos(radians), Math.sin(radians));
     return {
-      position: new Vector(x, y),
-      velocity: new Vector(Math.cos(radians), Math.sin(radians))
+      id: uuidv4(),
+      position,
+      velocity
     };
   };
-  function moveBoids(boids2, speed, separation, alignment, cohesion) {
+  function moveBoids(boids2, speed, separation, alignment, cohesion, jitter, allowWrapping) {
     const centerSum = boids2.reduce((acc, boid) => {
       acc.x += boid.position.x;
       acc.y += boid.position.y;
       return acc;
     }, new Vector());
     for (const boid of boids2) {
-      const center = new Vector((centerSum.x - boid.position.x) / (boids2.length - 1), (centerSum.y - boid.position.y) / (boids2.length - 1));
+      const center = new Vector((centerSum.x - boid.position.x) / Math.max(boids2.length - 1, 1), (centerSum.y - boid.position.y) / Math.max(boids2.length - 1, 1));
       const v1 = new Vector((center.x - boid.position.x) / 100 * cohesion, (center.y - boid.position.y) / 100 * cohesion);
       let v2 = new Vector();
+      let v3 = new Vector();
       for (const other of boids2) {
         if (other === boid) {
           continue;
@@ -54,83 +133,61 @@
         if (boid.position.distanceTo(other.position) < separation) {
           v2 = v2.subtract(other.position.subtract(boid.position));
         }
-      }
-      let v3 = new Vector();
-      for (const other of boids2) {
-        if (other === boid) {
-          continue;
+        if (!allowWrapping) {
+          if (boid.position.x < 10) {
+            v2 = v2.subtract(new Vector(-10, 0));
+          } else if (boid.position.x > canvas.width - 10) {
+            v2 = v2.subtract(new Vector(10, 0));
+          }
+          if (boid.position.y < 10) {
+            v2 = v2.subtract(new Vector(0, -10));
+          } else if (boid.position.y > canvas.height - 10) {
+            v2 = v2.subtract(new Vector(0, 10));
+          }
         }
         v3 = v3.add(other.velocity.mult(alignment));
       }
-      v3 = v3.div(boids2.length - 1);
-      boid.velocity = boid.velocity.add(v1).add(v2).add(v3).toUnit();
+      v3 = v3.div(Math.max(boids2.length - 1, 1));
+      const direction = Math.atan2(boid.velocity.y, boid.velocity.x);
+      const newDirection = direction + Math.PI * (jitter * Math.random() * (Math.random() - 0.5));
+      const jitterVector = new Vector(Math.cos(newDirection), Math.sin(newDirection)).toUnit();
+      boid.velocity = boid.velocity.add(v1).add(v2).add(v3).add(jitterVector).toUnit();
       boid.position = boid.position.add(boid.velocity.mult(speed));
     }
   }
 
-  // src/canvas.ts
-  var canvas = document.querySelector("canvas");
-  document.addEventListener("resize", () => {
-  });
-  canvas.width = canvas.getBoundingClientRect().width;
-  canvas.height = canvas.getBoundingClientRect().height;
-  var ctx = canvas.getContext("2d");
-  function rotatePoint(point, center, angle) {
-    const sin = Math.sin(angle);
-    const cos = Math.cos(angle);
-    point.x -= center.x;
-    point.y -= center.y;
-    const xNew = point.x * cos - point.y * sin;
-    const yNew = point.x * sin + point.y * cos;
-    point.x = xNew + center.x;
-    point.y = yNew + center.y;
-  }
-  var drawBoid = (boid) => {
-    const direction = Math.atan2(boid.velocity.y, boid.velocity.x);
-    const length = 10;
-    const tip = {
-      x: boid.position.x + length,
-      y: boid.position.y
-    };
-    const leftTail = {
-      x: boid.position.x - length / 2,
-      y: boid.position.y - length / 2
-    };
-    const rightTail = {
-      x: boid.position.x - length / 2,
-      y: boid.position.y + length / 2
-    };
-    rotatePoint(tip, boid.position, direction);
-    rotatePoint(leftTail, boid.position, direction);
-    rotatePoint(rightTail, boid.position, direction);
-    ctx.strokeStyle = "white";
-    ctx.fillStyle = "white";
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.moveTo(tip.x, tip.y);
-    ctx.lineTo(leftTail.x, leftTail.y);
-    ctx.lineTo(rightTail.x, rightTail.y);
-    ctx.lineTo(tip.x, tip.y);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-  };
-
   // src/main.ts
   var speedEl = document.getElementById("speed");
+  var sizeEl = document.getElementById("size");
+  var populationEl = document.getElementById("population");
   var separationEl = document.getElementById("separation");
   var alignmentEl = document.getElementById("alignment");
   var cohesionEl = document.getElementById("cohesion");
+  var jitterEl = document.getElementById("jitter");
+  var allowWrappingEl = document.getElementById("allowWrapping");
+  var colorEl = document.getElementById("color");
   var boids = [];
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < Number(populationEl.value); i++) {
     boids.push(createBoid(Math.random() * canvas.width, Math.random() * canvas.height));
   }
   function update(delta) {
     const speed = Number(speedEl.value);
+    const population = Number(populationEl.value);
     const separation = Number(separationEl.value);
     const alignment = Number(alignmentEl.value) / 100;
     const cohesion = Number(cohesionEl.value) / 100;
-    moveBoids(boids, speed * delta, separation, alignment, cohesion);
+    const jitter = Number(jitterEl.value) / 100;
+    const allowWrapping = !!allowWrappingEl.checked;
+    while (boids.length > population) {
+      boids.pop();
+    }
+    while (boids.length < population) {
+      boids.push(createBoid(Math.random() * canvas.width, Math.random() * canvas.height));
+    }
+    moveBoids(boids, speed * delta, separation, alignment, cohesion, jitter, allowWrapping);
+    if (!allowWrapping) {
+      return;
+    }
     for (const boid of boids) {
       if (boid.position.x <= 0) {
         boid.position.x = canvas.width + boid.position.x;
@@ -145,9 +202,12 @@
     }
   }
   function render() {
+    const size = Number(sizeEl.value);
+    const color = colorEl.value;
+    const allowWrapping = !!allowWrappingEl.checked;
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     for (const boid of boids) {
-      drawBoid(boid);
+      drawBoid(boid, size, color, allowWrapping);
     }
   }
   var prev = +new Date();
